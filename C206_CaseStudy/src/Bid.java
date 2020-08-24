df4e013f85812c895a2457fce039ae9966c18af9
19042228 <19042228@LAPTOP-6SP4S771>
2020-08-24 13:55:34 +0800

    View,Add,Delete

--- C206_CaseStudy/src/Bid.java
+++ C206_CaseStudy/src/Bid.java
@@ -1,4 +1,67 @@
 
 public class Bid {
 
+	private String bidID, itemName, sellerEmal, buyerEmail;
+	private double bidPrice;
+	
+	public Bid(String bidID, String itemName, String sellerEmal, String buyerEmail, double bidPrice) {
+		super();
+		this.bidID = bidID;
+		this.itemName = itemName;
+		this.sellerEmal = sellerEmal;
+		this.buyerEmail = buyerEmail;
+		this.bidPrice = bidPrice;
+	}
+
+	public String getBidID() {
+		return bidID;
+	}
+
+	public void setBidID(String bidID) {
+		this.bidID = bidID;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getSellerEmal() {
+		return sellerEmal;
+	}
+
+	public void setSellerEmal(String sellerEmal) {
+		this.sellerEmal = sellerEmal;
+	}
+
+	public String getBuyerEmail() {
+		return buyerEmail;
+	}
+
+	public void setBuyerEmail(String buyerEmail) {
+		this.buyerEmail = buyerEmail;
+	}
+
+	public double getBidPrice() {
+		return bidPrice;
+	}
+
+	public void setBidPrice(double bidPrice) {
+		this.bidPrice = bidPrice;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Bid [bidID=" + bidID + ", itemName=" + itemName + ", sellerEmal=" + sellerEmal + ", buyerEmail="
+				+ buyerEmail + ", bidPrice=" + bidPrice + "]";
+	}
+	
+	
+	
+	
+	
 }
--- C206_CaseStudy/src/BidDB.java
+++ C206_CaseStudy/src/BidDB.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
 
 public class BidDB {
-
+	
+	private static ArrayList<Bid> bidList = new ArrayList<Bid>();
+	
+	public static void addBid(Bid bid) {
+		bidList.add(bid);
+	}
+	
+	public static void viewAllBid() {
+		for (int i =0; i<bidList.size();i++) {
+			System.out.println("Bid " + i + ":");
+			System.out.println("ID: " + bidList.get(i).getBidID());
+			System.out.println("Item Name: " + bidList.get(i).getItemName());
+			System.out.println("Seller Email: " + bidList.get(i).getSellerEmal());
+			System.out.println("Buyer Email: " + bidList.get(i).getBuyerEmail());
+			System.out.println("Bid Price: " + bidList.get(i).getBidPrice());
+			System.out.println();
+		}
+	}
+	
+	public static void delBid(String a) {
+		int c = Integer.parseInt(a);
+		bidList.remove(c-1);
+	}
+	
+	public static void showBidMenu() {
+		System.out.println("Option 1: View All Bid");
+		System.out.println("Option 2: Add Bid");
+		System.out.println("Option 3: Remove Bid");
+	}
 }
